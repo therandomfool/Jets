b43b0032fcf877d72c25301c757c5122f6001fff
therandomfool <mddenver@protonmail.com>
2020-04-03 20:33:25 -0700

    Menu Work, Knock Out Low Hanging Fruit

--- src/com/skilldistillery/JetsApplication.java
+++ src/com/skilldistillery/JetsApplication.java
@@ -5,19 +5,38 @@ import java.util.*;
 public class JetsApplication {
 
 	Scanner input = new Scanner(System.in);
-	JetsApplication aifField = new JetsApplication();
-	
+	private AirField airField = new AirField();
 
 	public static void main(String[] args) {
+		JetsApplication jaApp = new JetsApplication();
 
 	}
-
 
 	private void launch() {
-		
-	} 
-	
-	private void UserMenu() {
-		
+
 	}
+
+	private void UserMenu() {
+		boolean menu = true;
+		while (menu) {
+		}
+	}
+
+	private void showMenu() {
+		StringBuilder choice = new StringBuilder(
+				"\n| \uD83D\uDE81 \t  \uD83D\uDEE9 |\n\n" 
+						+ "1.  List fleet\n" 
+						+ "2.  Fly all jets\n"
+						+ "3.  View fastest jet\n" 
+						+ "4.  View aircraft with longest range\n"
+						+ "5.  Load all cargo jets\n" 
+						+ "6.  Dogfight!\n"
+						+ "7.  Add aircraft to fleet\n" 
+						+ "8. Remove aircraft from fleet\n" 
+						+ "9. Quit"
+						+ "\n| \uD83D\uDE81 \t  \uD83D\uDEE9 |\n\n" );
+
+
+	}
+
 }
